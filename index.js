9ff890832803c1ad3ce813138f9d8cb0cd22462e
Kai Hilbourne <kaihilbourne@umail.ucsb.edu>
2024-06-14 21:44:48 -0700

    socket io working

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-  connectionStateRecovery: {}
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET","POST"]
+  }
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
-});
+app.use(cors());
+
+// app.get('/', (req, res) => {
+//   res.sendFile(join(__dirname, 'index.html'));
+// });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -26,7 +32,8 @@ io.on('connection', (socket) => {
   id += 1;
   let uid = "user" + id.toString();
   socket.on('chat message', (msg) => {
-    io.emit('chat message',msg,uid)
+    io.emit('chat message',msg+uid);
+    console.log(msg);
   });
 });
 
